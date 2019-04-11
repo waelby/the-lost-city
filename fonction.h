07c4105e6602e98fc761a8e2a42ed4a58cbc340d
ouma1212 <47774170+ouma1212@users.noreply.github.com>
2019-04-10 17:27:44 -0700

    Update fonction.h

--- fonction.h
+++ fonction.h
@@ -1,46 +1,99 @@
-#ifndef JEUX
-#define JEUX
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <SDL/SDL.h>
+#include  <stdlib.h>
+#include  <SDL/SDL.h>
 #include <SDL/SDL_image.h>
-#include <SDL/SDL_mixer.h>
-#include <SDL/SDL_ttf.h>
-typedef struct
-{
-SDL_Surface *screen;
-SDL_Surface *image;
-SDL_Rect positionecran,positionecran1;
-}ecran;
+#include "jeux.h"
 
-typedef struct 
-{
-TTF_Font *font;
-SDL_Surface *score;
-SDL_Rect position_score;
-int sscore;
-int vie;
-TTF_Font *fonte;
-SDL_Surface * temp;
-SDL_Rect postime;
-int suiv,prec,tmp;
-}score;
+SDL_Surface * Init_imagedefond( ecran *image ,ecran *positionecran ,  int x , int y) 
+ {
+	       ecran.image =  IMG_Load("map.png"); 
 
-typedef struct
-{
-SDL_Surface* imageper;
-SDL_Rect posper;
-}perso;
-typedef struct
-{
-SDL_Surface* imagerock;
-SDL_Rect posrock;
-}rock;
+	      if  ( ecran.image==  NULL )  { 
+         printf ( "Can not load image of tux: %s \n " , SDL_GetError ()); 
+         exit ( 1 ); 
+                  } 
+     else 
+     {
+ 
+     positionecran->x  =  x ; 
+     positionecran->y  = y ;
+     positionecran->h= ecran.image->h ;
+     positionecran->w= ecran.image->w ;  
+       }
+     
+     return (ecran.image) ; 
+}
+
+ void Show_imagedefond (ecran *image, SDL_Surface *screen,ecran positionecran ) 
+  {
+
+	 SDL_BlitSurface(ecran.image, NULL, ecran.screen, &ecran.positionecran);
+   }
 
 
-void initialiser_score(score *score,ecran ecran);
-int vie(score *score, int v);
-int collision (perso *p, rock *r);
-void deplacement(perso *p,SDL_Event event,int done,int *detection,rock *r,SDL_Event *felsa ,ecran ecran);
-void times(score *score,ecran ecran);
-#endif
+
+      /* Quitte la SDL */
+      SDL_Quit();
+
+
+void initialiser_score(score *score,ecran ecran)
+{
+TTF_Init();
+score->font=TTF_OpenFont("BebasNeue-Book.ttf", 50);
+SDL_Color color={255,0,0};
+char ch[10];
+score->sscore;
+sprintf(ch,"%d XP",score->sscore);
+score->score=TTF_RenderText_Solid(score->font,ch,color);
+SDL_BlitSurface(score->score,NULL,ecran.screen,&score->position_score);
+SDL_Delay(1000);
+SDL_Flip(ecran.screen);
+TTF_Quit();
+}
+
+int vie(score *score , int v)
+{
+    if (score->vie==1)
+          {
+         if(v<3)
+                v++;
+          }
+      else
+          if(score->vie==-1)
+              v--;
+                 else
+                    return v;
+ return v;
+}
+
+
+
+void times(score *score,ecran ecran)
+{
+TTF_Init();
+char ch[50];
+score->fonte=TTF_OpenFont("BebasNeue-Book.ttf", 70);
+SDL_Color color={255,0,0};
+score->suiv = SDL_GetTicks();
+if (score->suiv - score->prec>=1000)
+{
+score->tmp ++;
+if (score->tmp<10)
+{
+sprintf(ch,"00:0%d",score->tmp);
+}
+else if (score->tmp==10)
+{
+sprintf(ch,"00:%d",score->tmp);
+}
+score->prec=score->suiv;
+}
+score->temp=TTF_RenderText_Solid(score->fonte,ch,color);
+SDL_BlitSurface(score->temp,NULL,ecran.screen,&score->postime);
+if (score->tmp==10)
+{ 
+score->tmp = 0 ;
+TTF_Quit();
+}
+}
